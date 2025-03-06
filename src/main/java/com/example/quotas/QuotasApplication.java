c90896b28f1c9820c0d6b287c3b9216eb12488f7
“simaocampos87” <simao.campos@eupago.pt>
2025-03-06 17:10:44 +0000

    second commit

---
+++ src/main/java/com/example/quotas/QuotasApplication.java
@@ -0,0 +1,13 @@
+package com.example.quotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotasApplication.class, args);
+	}
+
+}
